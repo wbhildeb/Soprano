8cd0560db5f8d354f9d9679045928ef88d606c50
Walker Hildebrand <walker99113@gmail.com>
2019-02-06 17:36:30 -0500

    Setup app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PaperShowcaseComponent } from './components/paper-showcase/paper-showcase.component';
+import { SpotifyComponent } from './components/spotify/spotify.component';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'paper', component: PaperShowcaseComponent},
+  { path: 'spotify', component: SpotifyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SpotifyComponent } from './components/spotify/spotify.component';
+import { PaperShowcaseComponent } from './components/paper-showcase/paper-showcase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SpotifyComponent,
+    PaperShowcaseComponent
   ],
   imports: [
     BrowserModule,
