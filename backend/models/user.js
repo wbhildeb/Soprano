20bc66e589559e54681cf4455ce8bb0beff2a281
Walker Hildebrand <walker99113@gmail.com>
2019-02-09 17:18:37 -0500

    create user model

---
+++ backend/models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    id: { type: String, required: true},
+    name: { type: String, required: true },
+    imageURL: { type: String, default: 'https://cdn.pixilart.com/photos/large/50909fee485e6aa.jpg' },
+});
+
+module.exports = mongoose.model('User', userSchema);
