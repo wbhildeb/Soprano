96eb8d63682baad84a49643e944b60e45d9b0ce3
Walker Hildebrand <walker99113@gmail.com>
2019-02-24 14:32:41 -0500

    Modify user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    userID:         { type: String, required: true },
-    displayName:    { type: String, required: true },
-    imageURL:       { type: String, required: true },
+    country:        { type: String, required: false },
+    display_name:   { type: String, required: false },
+    href:           { type: String, required: false },
+    id:             { type: String, required: false },
+    image_url:      { type: String, required: false },
+    product:        { type: String, required: false },
+    type:           { type: String, required: false },
+    uri:            { type: String, required: false },
 });
 
 module.exports = mongoose.model('User', userSchema);
