2394c48baddc0bb70ce67f06910de7b1b8e7e506
Walker Hildebrand <walker99113@gmail.com>
2019-03-10 16:40:50 -0400

    Add projects

--- src/app/components/projects/project-data.ts
+++ src/app/components/projects/project-data.ts
@@ -2,6 +2,11 @@ import { Project } from './project';
 
 export const PROJECTS : Project[] = [
     new Project({
+        title: 'EasyDocs',
+        technologies: [ 'Python', 'SQLite', 'HTML', 'CSS', 'JavaScript' ],
+        repositoryURL: 'https://github.com/wbhildeb/EasyDocs'
+    }),
+    new Project({
         title: 'Multiquadris',
         technologies: [ 'C++' ],
         repositoryURL: 'https://github.com/wbhildeb/Multiquadris',
@@ -9,10 +14,17 @@ export const PROJECTS : Project[] = [
     }),
     new Project({
         title: 'Localize CS',
-        technologies: [ 'Bash' ]
+        technologies: [ 'Bash' ],
+        repositoryURL: 'https://github.com/wbhildeb/localize-cs'
     }),
     new Project({
-        title: 'Poker Stats Calculator',
-        technologies: [ 'Java', 'SQL', ]
+        title: 'Poker Stats',
+        technologies: [ 'Java', 'SQL', ],
+        repositoryURL: 'https://github.com/wbhildeb/Texas-Hold-em-Calculator'
     }),
+    new Project({
+        title: 'Ride the Bus',
+        technologies: [ 'C#', ".NET" ],
+        repositoryURL: 'https://github.com/wbhildeb/Ride-The-Bus'
+    })
 ];
