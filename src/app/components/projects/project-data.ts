a459546e8d1fa87a8bb51edde9cbdb8e9d16c23e
Walker Hildebrand <walker99113@gmail.com>
2019-03-09 20:09:00 -0500

    Add basic project data

---
+++ src/app/components/projects/project-data.ts
@@ -0,0 +1,32 @@
+import { Project } from './project';
+
+export const PROJECTS : Project[] = [
+    {
+        id: 0,
+        title: 'Multiquadris',
+        technologies: [ 'C++' ],
+        repositoryURL: 'https://github.com/wbhildeb/Multiquadris',
+        descriptionHTML: 'multiquadris.htm'
+    },
+    {
+        id: 1,
+        title: 'Localize CS',
+        technologies: [ 'Bash' ],
+        repositoryURL: '',
+        descriptionHTML: ''
+    },
+    {
+        id: 2,
+        title: 'Poker Statistics Calculator',
+        technologies: [ 'Java' ],
+        repositoryURL: '',
+        descriptionHTML: ''
+    },
+    {
+        id: 3,
+        title: 'Localize CS',
+        technologies: [ 'Bash' ],
+        repositoryURL: '',
+        descriptionHTML: ''
+    }
+];
