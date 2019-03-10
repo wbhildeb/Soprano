d4508cfb8ddb1a910fb101ed8c0bd74a2a24abcb
Walker Hildebrand <walker99113@gmail.com>
2019-03-09 20:09:55 -0500

    Remove unused import

--- src/app/components/projects/project/project.component.ts
+++ src/app/components/projects/project/project.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { projectionDef } from '@angular/core/src/render3';
+
 
 @Component({
   selector: 'app-project',
