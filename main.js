7e00958700929776ffb7bdc0d3288a5506d7709a
Walker Hildebrand <walker99113@gmail.com>
2019-03-11 19:19:24 -0400

    Remove test

--- main.js
+++ main.js
@@ -3,10 +3,4 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist/personal-site'));
-
-app.get('/test', (req, res, next) =>
-{
-    res.end('test complete');
-});
-
 app.listen(process.env.PORT || 8080);
