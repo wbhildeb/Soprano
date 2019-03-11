67d1ac3ec56734a8f509730c613d0a8e1e08f612
Walker Hildebrand <walker99113@gmail.com>
2019-03-11 19:18:26 -0400

    Fix site directory

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/personal-site'));
 
 app.get('/test', (req, res, next) =>
 {
