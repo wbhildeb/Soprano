2ac886f76cd39608ca5d1947d39c4c1aa0e4730d
Walker Hildebrand <walker99113@gmail.com>
2019-03-11 19:29:03 -0400

    allow angular routing

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist/personal-site'));
+
+app.get('/*', (req, res, next) =>
+{
+    res.sendFile(__dirname + '/dist/personal-site/index.html');
+});
 app.listen(process.env.PORT || 8080);
