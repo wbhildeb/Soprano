11419ce2bb172bebd8c6195e0f6a0ef10522c77a
Walker Hildebrand <walker99113@gmail.com>
2019-03-11 19:08:06 -0400

    add a test

--- main.js
+++ main.js
@@ -3,4 +3,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
+
+app.get('/test', (req, res, next) =>
+{
+    res.end('test complete');
+});
+
 app.listen(process.env.PORT || 8080);
