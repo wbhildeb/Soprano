8e1009b885e4ccbb4596d95e96a1f84be6dd6a29
Walker Hildebrand <walker99113@gmail.com>
2019-10-07 21:45:28 -0400

    Setup authentication framework

--- backend/app.js
+++ backend/app.js
@@ -2,77 +2,67 @@
  * app.js
  * 
  * Walker Hildebrand
- * 2019-09-13
+ * 2019-10-07
  * 
  */
 
 ///////// Imports /////////////////////////////////////////
-const mongoose = require('mongoose');
+const express = require('express');
+const session = require('express-session');
+const querystring = require('querystring');
 
-
-///////// Models //////////////////////////////////////////
-import { PlaylistPair, Session, User } from './models';
-
-
-///////// Database ////////////////////////////////////////
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() =>
-    {
-        console.log('Database connection successful');
-    })
-    .catch(err => {
-        console.log('Database connection failed');
-        console.log(err);
-    });
-
-
-///////// Spotify ////////////////////////////////////////
-const saveSession = function(session)
-{
-    new Promise()
-    return new Promise((resolve, reject) => {
-        Session
-            .deleteMany({ sessionID: session.sessionID })
-            .then(() =>
-            {
-                console.log('Saved ' + JSON.stringify(session));
-                session.save((other, err) =>
-                {
-                    if (err)
-                    {
-                        console.log(err);
-                        reject(err);
-                    }
-                    else
-                    {
-                        resolve(other);
-                    }
-                });
-            })
-            .catch(err =>
-            {
-                console.log(err)
-                reject(err);
-            });
-    });
+const SPOTIFY_INFO = {
+    AuthorizeLink: 'https://accounts.spotify.com/authorize?',
+    ClientID: '391e2916ad4a4709908a2d71ffaeb0c5',
+    Scope: 'user-library-modify user-read-private user-modify-playback-state user-read-currently-playing user-read-recently-played user-modify-playback-state user-read-private',
+    RedirectURI: 'http://localhost:3000/spotify/callback'
 }
 
-const getUserFromSession = function(session)
-{
-    return new Promise((resolve, reject) =>
-    {
-        Session
-            .findOne({ sessionID: session.sessionID })
-    })
-}
+const app = express();
 
+app
+    .use(session({
+        secret: 'gotta go home',
+        resave: false,
+        saveUninitialized: true
+    }));
 
-///////// Debug ///////////////////////////////////////////
-const printSessions = function()
+// Debugging
+app.get('*', (req, res, next) =>
 {
-    Session.find({}, (err, docs) =>
-    {
-        if (err) console.log(err);
-        console.log(docs);
+    console.log(`request type:  ${req.method}`);
+    console.log(`url:           ${req.url}`);
+    console.log(`session:       ${req.sessionID}`);
+    console.log('-----------------------------------------------');
+    next();
+});
+
+app.get('/spotify/login', (req, res, next) =>
+{
+    const query = querystring.stringify({
+        response_type: 'code',
+        client_id: SPOTIFY_INFO.ClientID,
+        scope: SPOTIFY_INFO.Scope,
+        redirect_uri: SPOTIFY_INFO.RedirectURI,
+        state: req.sessionID
     });
-}
+
+    res.redirect(SPOTIFY_INFO.AuthorizeLink + query);
+});
+
+app.get('/spotify/callback', (req, res, next) =>
+{
+    const state = req.query.state;
+    const code = req.query.code;
+
+    if (req.query.state != req.sessionID)
+    {
+        console.error(`Mismatched state error - session: ${re1.sessionID}, state: ${state}`);
+        res.redirect('/error');
+    }
+    
+    console.log(code);
+    res.send();
+});
+
+app.listen(3000)
