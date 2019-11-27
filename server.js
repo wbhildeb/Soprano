9e0d6da09a503f0d797521b227457b1e9c20b956
Walker Hildebrand <walker99113@gmail.com>
2019-11-27 00:23:02 -0500

    Add body parser

--- server.js
+++ server.js
@@ -24,13 +24,13 @@ const spotify = spotifyWrapper();
 //db.DeleteSessionData(); TODO: Uncomment at some point
 
 app
+  .use(express.bodyParser())
   .use(session({
     secret: 'gotta go home',
     store: new FileStore({}),
     resave: false,
     saveUninitialized: true,
   }))
-  .use('/api/foosboard', foosboard)
   .use(
     (req, res, next) =>
     {
@@ -39,13 +39,14 @@ app
       res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Request-With, Content-Type, Accept'
-      );
-      res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS'
-      );
-      next();
-    });
+        );
+        res.setHeader(
+          'Access-Control-Allow-Methods',
+          'GET, POST, PATCH, DELETE, OPTIONS'
+          );
+          next();
+    })
+  .use('/api/foosboard', foosboard);
 
 // Debugging
 app.use('*', (req, res, next) => 
