de5150133b8fc50f54b1102c530b057bf2bb2926
Walker Hildebrand <walker99113@gmail.com>
2020-01-15 15:05:23 -0500

    Remove TestTree component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SubPlaylistsComponent } from './components/spotify/sub-playlists/sub-playlists.component';
-import { TestTreeComponent } from './components/test/test-tree/test-tree.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
     { path: 'dashboard', component: DashboardComponent },
     { path: 'sub-playlists', component: SubPlaylistsComponent },
-    { path: 'test/tree', component: TestTreeComponent },
 ];
 
 @NgModule({
