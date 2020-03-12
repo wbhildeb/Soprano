9a92d63c9cdf51567cafc79a6deb88acead55b8c
Sara <shhabdel@uwaterloo.ca>
2020-03-12 11:58:12 -0400

    Update database.js
    Cleanup

--- backend/soprano/database.js
+++ backend/soprano/database.js
@@ -21,20 +21,12 @@ class Database
 
   /** 
    * Signs in users as anonymous to grant read/write access to the database
-   * Todo later: allow users to only access their data*/
+   * TODO later: allow users to only access their data
+  */
   SignIn() 
   {
     firebase.auth().signInAnonymously()
-      .then(() => 
-      {
-        console.log('Logged in as Anonymous!');
-      }).catch((error) => 
-      {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
+      .catch(console.error);
   }
 
   /**
